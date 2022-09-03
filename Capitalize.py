7c83d176fae42f08e0cef9bbff4cab5c34ad26b0
Summeyyetsn <summeyyetsn@gmail.com>
2022-09-03 19:47:17 +0300

    Capitalize

---
+++ Capitalize.py
@@ -0,0 +1,46 @@
+"""
+Capitalize:
+You are asked to ensure that the first and last names of people begin with a capital letter in their passports.
+For example, alison heck should be capitalised correctly as Alison Heck.
+Given a full name, your task is to capitalize the name appropriately.
+Input Format
+A single line of input containing the full name, S.
+
+    Constraints:
+0<len(S)<1000
+The string consists of alphanumeric characters and spaces.
+Note: in a word only the first character is capitalized. Example 12abc when capitalized remains 12abc.
+
+Output Format
+Print the capitalized string, S.
+Sample Input : chris alan
+Sample Output : Chris Alan
+"""
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+    my_list= s.split(" ")
+    new_list=list()
+    for i in my_list:
+        item = i.capitalize()
+        new_list.append(item)
+    new_string= " ".join(new_list)
+    return new_string
+
+if __name__ == '__main__':
+    #fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+    print(result)
+    #fptr.write(result + '\n')
+
+    #fptr.close()
